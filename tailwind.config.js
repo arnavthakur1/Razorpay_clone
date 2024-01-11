2c6ae9447eadc417a78adad930a4b67da75c8aeb
arnavthakur1 <137320688+arnavthakur1@users.noreply.github.com>
2024-01-11 19:08:43 +0530

    Changes committed
    Created a Razorpay clone using HTML and Tailwind CSS.

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+      mullish:["Mullish","sans-serrif"],
+    },
+    colors: {
+        LightBlue: "#2b84ea",
+        LightBlue300: "#4b94ed",
+        LightBlue500: "#Ob72e7",
+        greenLight: "#61cea",
+        grayText: "#818597",
+        LightGray: "#e2eZe2",
+        grayBlue: "#344а6с", 
+        deepBlue: "#02042a",
+        deepBlueHead: "#16256",
+        gray2: "#525a76",
+      },
+    },
+  },
+  plugins: [],
+}
+
